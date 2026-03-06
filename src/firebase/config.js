998fb09bd5629bfd812ab10a3a55627d6a706f49
yasirdar19 <y.dar28@ncf.edu>
2026-03-05 20:54:33 -0500

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,19 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC2GhWX5gewjJBeoi9BbsNbhEr-N_Um2dI",
-  authDomain: "mini-project-05.firebaseapp.com",
-  projectId: "mini-project-05",
-  storageBucket: "mini-project-05.firebasestorage.app",
-  messagingSenderId: "611100054242",
-  appId: "1:611100054242:web:c2894e4719de558f4504bb",
-  measurementId: "G-HNQDJ33L2E"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-
 export const googleProvider = new GoogleAuthProvider();
 
 export default app;
