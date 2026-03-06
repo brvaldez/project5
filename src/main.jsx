aac7746a580034e9da9f13dd4443c2d6e2c9570c
brvaldez <b.valdez26@ncf.edu>
2026-03-05 20:33:45 -0500

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import { AuthProvider } from "./contexts/AuthContext";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-    <ToastContainer position="bottom-right" theme="dark" />
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+        <ToastContainer position="bottom-right" theme="dark" />
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>,
 );
